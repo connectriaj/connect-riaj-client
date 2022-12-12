302b1db4ab9273a7fc7ce107bc4cfe2baff3d726
connectriaj <connect.riaj@hotmail.com>
2022-12-13 04:31:09 +0600

    about section updated

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,11 +1,61 @@
 import React from "react";
 import "./About.css";
+import about from "../../assets/avatar-2.svg";
+import AboutBox from "./AboutBox";
 
 const About = () => {
   return (
-    <div>
-   
-    </div>
+    <section className="about container section" id="about">
+      <h2 className="section__title">About Me</h2>
+      <div className="about__container grid">
+        <img src={about} alt="about" className="about__image" />
+        <div className="about__data grid">
+          <div className="about__info">
+            <p className="about__description">
+              I am Bolby Doe, web developer from London, United Kingdom. I have
+              rich experience in web site design and building and customization,
+              also I am good at WordPress.
+            </p>
+            <a href="" className="btn">
+              Download CV
+            </a>
+          </div>
+
+          <div className="about__skills grid">
+            <div className="skills__data">
+              <div className="skills__titles">
+                <h3 className="skills__name ">Development</h3>
+                <span className="skills__number">90%</span>
+              </div>
+              <div className="skills__bar">
+                <span className="skills__percentage development"></span>
+              </div>
+            </div>
+
+            <div className="skills__data">
+              <div className="skills__titles">
+                <h3 className="skills__name">Ui-Design</h3>
+                <span className="skills__number">95%</span>
+              </div>
+              <div className="skills__bar">
+                <span className="skills__percentage ui__design"></span>
+              </div>
+            </div>
+
+            <div className="skills__data">
+              <div className="skills__titles">
+                <h3 className="skills__name">Mongo DB</h3>
+                <span className="skills__number">80%</span>
+              </div>
+              <div className="skills__bar">
+                <span className="skills__percentage mongo__db"></span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <AboutBox />
+    </section>
   );
 };
 
