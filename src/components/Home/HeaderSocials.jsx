ed256a38bf17d94bfb5f7627f3e483fa1413c764
connectriaj <connect.riaj@hotmail.com>
2022-12-13 03:29:23 +0600

    home page updated

---
+++ src/components/Home/HeaderSocials.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+const HeaderSocials = () => {
+  return (
+    <div className="home__socials">
+      <a
+        href="https://www.instagram.com"
+        className="home__social-link"
+        target="_blank"
+      >
+        <i className="fa-brands fa-instagram"></i>
+      </a>
+
+      <a
+        href="https://www.twitter.com"
+        className="home__social-link"
+        target="_blank"
+      >
+        <i className="fa-brands fa-twitter"></i>
+      </a>
+
+      <a
+        href="https://www.facebook.com"
+        className="home__social-link"
+        target="_blank"
+      >
+        <i className="fa-brands fa-facebook"></i>
+      </a>
+
+      <a
+        href="https://www.youtube.com"
+        className="home__social-link"
+        target="_blank"
+      >
+        <i className="fa-brands fa-youtube"></i>
+      </a>
+
+      <a
+        href="https://www.github.com"
+        className="home__social-link"
+        target="_blank"
+      >
+        <i className="fa-brands fa-github"></i>
+      </a>
+    </div>
+  );
+};
+
+export default HeaderSocials;
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import HeaderSocials from "./HeaderSocials";
+import "./Home.css";
+import ScrollDown from "./ScrollDown";
+import man from "../../assets/man.svg";
+import Shapes from "./Shapes";
+
+const Home = () => {
+  return (
+    <section className="home container" id="home">
+      <div className="intro">
+        <img src={man} alt="intro" className="home__image" />
+        <h1 className="home__name">Md Riajul Islam</h1>
+        <span className="home__education">I'm a MERN Stack Developer</span>
+        <HeaderSocials />
+        <a href="#contact" className="btn">
+          Hire Me
+        </a>
+        <ScrollDown />
+      </div>
+      <Shapes />
+    </section>
+  );
+};
+
+export default Home;
