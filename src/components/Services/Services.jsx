7c5eda82fc427c83ab655a51c7c1b29a2b99c93b
connectriaj <connect.riaj@hotmail.com>
2022-12-13 15:17:50 +0600

    services section updated

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,12 +1,51 @@
-import React from 'react';
-import './Services.css'
+import React from "react";
+import "./Services.css";
+import service1 from "../../assets/service-1.svg";
+import service2 from "../../assets/service-2.svg";
+import service3 from "../../assets/service-3.svg";
+
+const data = [
+  {
+    id: 1,
+    image: service1,
+    title: "UI/UX design",
+    description:
+      "Lorem ipsum dolor sit amet consectetuer adipiscing elit         aenean commodo ligula eget.",
+  },
+  {
+    id: 2,
+    image: service2,
+    title: "Web Development",
+    description:
+      "Lorem ipsum dolor sit amet consectetuer adipiscing elit         aenean commodo ligula eget.",
+  },
+  {
+    id: 3,
+    image: service3,
+    title: "Photography",
+    description:
+      "Lorem ipsum dolor sit amet consectetuer adipiscing elit         aenean commodo ligula eget.",
+  },
+];
 
 const Services = () => {
-    return (
-        <div>
-           
-        </div>
-    );
+  return (
+    <section className="services container section" id="services">
+      <h2 className="section__title">Services</h2>
+
+      <div className="services__container grid">
+        {data.map(({ id, image, title, description }) => {
+          return (
+            <div className="services__card" key={id}>
+              <img src={image} alt="photo" className="services__image" />
+              <h3 className="services__title">{title}</h3>
+              <p className="services__description">{description}</p>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Services;
